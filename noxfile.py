b44088fcbf9b0521b327b603ed17da3f56d739cb
John Beisley <johnbeisley@proton.me>
2026-04-27 18:36:54 +0100

    feat: set up basic Python dev environment.

---
+++ noxfile.py
@@ -0,0 +1,16 @@
+import nox
+import nox_uv
+
+nox.options.default_venv_backend = "uv"
+
+
+@nox_uv.session(uv_groups=["dev"])
+def check(session: nox.Session) -> None:
+    session.run("ruff", "check")
+    session.run("ty", "check")
+
+
+@nox_uv.session(uv_groups=["dev"])
+def fix(session: nox.Session) -> None:
+    session.run("isort")
+    session.run("ruff", "format")
